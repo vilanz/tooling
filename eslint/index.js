c46516f5bb0040f3d3af2dddd6394dc1e0e9d252
vilanz <vs.lanzarini@gmail.com>
2021-06-02 13:08:58 -0300

    prefer named exports

--- eslint/index.js
+++ eslint/index.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: ['airbnb-typescript', 'airbnb/hooks', 'prettier'],
   ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+  },
 };
