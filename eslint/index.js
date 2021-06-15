72662ca00cbff8fd94ac493cf080fdab0bbde847
Vinicius Lanzarini <vs.lanzarini@gmail.com>
2021-06-15 19:54:35 -0300

    ignoring more config files

--- eslint/index.js
+++ eslint/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['airbnb-typescript', 'airbnb/hooks', 'prettier'],
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '.prettierc.js', 'jest.config.js'],
   rules: {
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
